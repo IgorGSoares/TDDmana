91eec9f333fb7049d320cb1bae698952acc3cbb0
IgorGSoares <69635754+IgorGSoares@users.noreply.github.com>
2021-05-08 05:22:58 -0300

    teste-2
    integração das partes e melhoramentos

--- manaB.js
+++ manaB.js
@@ -3,6 +3,12 @@ let potion = 10;
 let max = 30;
 let upgradeV = 10;
 
+let valorMagiaFraca = 5;
+let valorMagiaMedia = 10;
+let valorMagiaForte = 15;
+
+module.exports = {};
+
 module.exports.getValue = () =>{
     return ManaV;
 };
@@ -35,4 +41,48 @@ module.exports.reset = () =>{
 module.exports.upgrade = () =>{
     max += upgradeV;
     ManaV = (max-ManaV)+ManaV;
-}
+};
+
+
+module.exports.getValorMagiaFraca = () => {
+    return valorMagiaFraca;
+};
+  
+module.exports.getValorMagiaMedia = () => {
+    return valorMagiaMedia;
+};
+  
+module.exports.getValorMagiaForte = () => {
+    return valorMagiaForte;
+};
+
+module.exports.usarMagia = (tipo) => {
+    if (ManaV <= 0) {
+        ManaV = 0;
+        return ManaV;
+    }else if (tipo === "fraca") {
+       if (ManaV < valorMagiaFraca) {
+            return ManaV;
+       }
+       else{
+            module.exports.setValue(ManaV - valorMagiaFraca);
+            return ManaV;
+       }
+    } else if (tipo === "media") {
+        if (ManaV < valorMagiaMedia) {
+            return ManaV;
+        }
+        else{
+            module.exports.setValue(ManaV - valorMagiaMedia);
+            return ManaV;
+        }
+    } else if (tipo === "forte") {
+        if (ManaV < valorMagiaForte) {
+            return ManaV;
+        }
+        else{
+            module.exports.setValue(ManaV - valorMagiaForte);
+            return ManaV;
+        }
+    }
+};
--- manaB.test.js
+++ manaB.test.js
@@ -9,17 +9,21 @@ test("setar mana", () =>{
     expect(mana.getValue()).toBe(15);
 });
 
+//========carga, descarga e recarga=======
 test ("usar poção de mana", () =>{
+    mana.setValue(15);
     mana.usePotion();
     expect(mana.getValue()).toBe(25);
 });
 
 // test ("usar poção de mana", () =>{
+//     mana.setValue(25);
 //     mana.usePotion();
 //     expect(mana.getValue()).toBe(35);
 // });
 
 test ("usar poção de mana 2", () =>{
+    mana.setValue(25);
     mana.usePotion();
     expect(mana.getValue()).toBe(30);
 });
@@ -36,3 +40,76 @@ test ("upar mana", () =>{
     mana.reset();
     expect(mana.getValue()).toBe(40);
 })
+
+//=================================
+
+
+//========magias===================
+
+test("usar magia fraca", () => {
+    let valorInicial = mana.getValue();
+    expect(mana.usarMagia("fraca")).toBe(
+      valorInicial - mana.getValorMagiaFraca()
+    );
+  });
+  
+  test("usar magia média", () => {
+    let valorInicial = mana.getValue();
+    expect(mana.usarMagia("media")).toBe(
+      valorInicial - mana.getValorMagiaMedia()
+    );
+  });
+  
+  test("usar magia forte", () => {
+    let valorInicial = mana.getValue();
+    expect(mana.usarMagia("forte")).toBe(
+      valorInicial - mana.getValorMagiaForte()
+    );
+  });
+
+//   test("usar magia sem mana", () =>{
+//     mana.setValue(0);
+//     let valorInicial = mana.getValue();
+//     expect(mana.usarMagia("forte")).toBe(
+//       valorInicial - mana.getValorMagiaForte()
+//     );
+//   });
+
+test("usar magia sem mana", () =>{
+    mana.setValue(0);
+    let valorInicial = mana.getValue();
+    expect(mana.usarMagia("forte")).toBe(0);
+  });
+
+//   test("mana insuficiente para magia fraca", () =>{
+//     mana.setValue(3);
+//     let valorInicial = mana.getValue();
+//     expect(mana.usarMagia("fraca")).toBe(valorInicial - mana.getValorMagiaFraca());
+//   });
+
+test("mana insuficiente para magia fraca", () =>{
+    mana.setValue(3);
+    let valorInicial = mana.getValue();
+    expect(mana.usarMagia("fraca")).toBe(mana.getValue());
+  });
+
+  test("mana insuficiente para magia media", () =>{
+    mana.setValue(9);
+    let valorInicial = mana.getValue();
+    expect(mana.usarMagia("media")).toBe(mana.getValue());
+  });
+
+  test("mana insuficiente para magia forte", () =>{
+    mana.setValue(13);
+    let valorInicial = mana.getValue();
+    expect(mana.usarMagia("forte")).toBe(mana.getValue());
+  });
+
+  test("poção e magia", () =>{
+    mana.setValue(5);
+    mana.usePotion();
+    let valorInicial = mana.getValue();
+    expect(mana.usarMagia("forte")).toBe(valorInicial - mana.getValorMagiaForte());
+  });
+
+  //=======================================
